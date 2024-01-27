5ac2c04cb7984af76a506a6a88ec4b7724759777
PaysanCorrezien <108607151+PaysanCorrezien@users.noreply.github.com>
2024-01-27 22:30:32 +0100

    trying to win some ms

--- lua/yazi.lua
+++ lua/yazi.lua
@@ -25,35 +25,41 @@ end
 
 local function on_exit(job_id, code, event)
 	if code ~= 0 then
+		print("Yazi exited with code: " .. code)
 		return
 	end
-
-	-- test
-	-- local file=io.open("/tmp/test.txt","w+")
-	-- io.output(file)
-	-- io.write(code)
-	-- io.close()
 
 	YAZI_BUFFER = nil
 	YAZI_LOADED = false
 	vim.g.yazi_opened = 0
 	vim.cmd("silent! :checktime")
 
-	if vim.api.nvim_win_is_valid(prev_win) then
+	if vim.api.nvim_win_is_valid(win) then
 		vim.api.nvim_win_close(win, true)
-		vim.api.nvim_set_current_win(prev_win)
-		if code == 0 and file_exists(output_path) == true then
-			local chosen_file = vim.fn.readfile(output_path)[1]
-			if chosen_file then
-				vim.cmd(string.format("edit %s", chosen_file))
+
+		if code == 0 and file_exists(output_path) then
+			local file_lines = vim.fn.readfile(output_path)
+			local chosen_file = file_lines and file_lines[1]
+
+			if chosen_file and chosen_file ~= "" then
+				vim.cmd(string.format("edit %s", vim.fn.fnameescape(chosen_file)))
+
+				coroutine.wrap(function()
+					if vim.api.nvim_buf_is_valid(buffer) and vim.api.nvim_buf_is_loaded(buffer) then
+						vim.api.nvim_buf_delete(buffer, { force = true })
+					end
+				end)()
+			else
+				print("File chosen is empty or invalid.")
 			end
+		else
+			print("Exit code is not 0 or output file does not exist")
 		end
-		prev_win = -1
-		if vim.api.nvim_buf_is_valid(buffer) and vim.api.nvim_buf_is_loaded(buffer) then
-			vim.api.nvim_buf_delete(buffer, { force = true })
-		end
+
 		buffer = -1
 		win = -1
+	else
+		print("Window is not valid at the time of closing.")
 	end
 end
 
@@ -63,6 +69,8 @@ local function exec_yazi_command(cmd)
 	if YAZI_LOADED == false then
 		-- ensure that the buffer is closed on exit
 		vim.g.yazi_opened = 1
+		-- vim.fn.termopen({ on_exit = on_exit })
+		-- vim.fn.termopen(cmd, { on_exit = on_exit })
 		vim.fn.termopen(cmd, { on_exit = on_exit })
 	end
 	vim.cmd("startinsert")
@@ -89,6 +97,7 @@ local function yazi(path)
 	-- 	end
 	-- end
 
+	-- TODO: do this better to not wait io
 	os.remove(output_path)
 	local cmd = string.format('yazi "%s" --chooser-file "%s"', path, output_path)
 
